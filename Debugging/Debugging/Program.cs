f1daf1860a334ae896481afbb98ce3d071df13d2
Victor Gavrish <victor_gavrish@epam.com>
2016-02-17 19:01:51 +0300

    Keygen for the crackme task

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Debugging
+{
+    using System;
+    using System.Linq;
+    using System.Net.NetworkInformation;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serial = GetSerial();
+
+            Console.WriteLine(serial);
+
+            Console.ReadLine();
+        }
+
+        private static string GetSerial()
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces().First();
+            var addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+            var dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            var numArray =
+                addressBytes.Select((b, index) => b ^ dateBytes[index]).Select(x => x < 999 ? x * 10 : x).ToArray();
+
+            var result = string.Join("-", numArray.Select(i => i.ToString()));
+
+            return result;
+        }
+    }
+}
